2d7a6342e6ff5f9443537e44b438bf834846e7e2
Sathya P <sathyasoft123@gmail.com>
2023-08-04 20:26:35 +0530

    Initial commit - By Sathya

---
+++ gitAppVariousStages/gitAppVariousStages/main.cpp
@@ -0,0 +1,63 @@
+//
+//  main.cpp
+//  gitAppVariousStages
+//
+//  Created by Sathya Babu on 04/08/23.
+//
+
+/*
+ 
+ 
+ */
+
+#include <iostream>
+#include<vector>
+using namespace std ;
+
+class MyNumber{
+    
+    public :
+    
+   
+    
+    MyNumber( int a , int b ){
+        cout << " Normal constructor " << a << " " << b << endl ;
+        
+    }
+    MyNumber( int a , int b , int c ){
+        cout << " Normal constructor " << a << " " << b   << " " << c << endl ;
+
+    }
+    void print(){
+        for(auto itm :  myVec){
+            cout << itm << " " ;
+            
+        }
+    }
+    
+    private :
+    
+    std::vector< int > myVec ;
+};
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Git code Basic one... With Constructors !\n";
+    
+//   depricated way of coding..
+    
+    
+    MyNumber myNum(10,20);
+    myNum.print();
+    
+    
+    
+    
+    return 0;
+}
+/*
+ 
+ Git code Basic one... With Constructors !
+  Normal constructor 10 20
+ 
+ */
