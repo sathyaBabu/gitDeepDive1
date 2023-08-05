204292a2467314ec7a58beeeef04f2080d83156f
Sathya P <sathyasoft123@gmail.com>
2023-08-05 07:18:53 +0530

    Implemented a new fun in anotherFile now -Sathya

--- anotherFile.cpp
+++ anotherFile.cpp
@@ -1,5 +1,12 @@
+
+addNewFun() {
+
+   println("\n Added a new fun. This part of the code will be a new commit."
+}
 main()
 {
   println("\n Added yet another file into the gitAppVariousStages folder \n");
+
+  addNewFun(); // this will go into new commit
 }
 
